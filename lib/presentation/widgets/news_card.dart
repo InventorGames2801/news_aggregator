17bf025dd582c37f1e72690a8e4e77c5dc9a1118
InventorGames2801 <ivan280104@gmail.com>
2026-02-05 10:46:47 +0800

    code refactoring

--- lib/presentation/widgets/news_card.dart
+++ lib/presentation/widgets/news_card.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import '../../domain/entities/news_article.dart';
 
 class NewsCard extends StatelessWidget {
