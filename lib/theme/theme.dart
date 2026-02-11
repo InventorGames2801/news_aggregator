397be6e6e6d52f08b86db71fa565c63177ceff3a
InventorGames2801 <ivan280104@gmail.com>
2026-02-11 10:36:10 +0800

    add text styles

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 final themeDark = ThemeData(
-  textTheme: TextTheme(bodySmall: TextStyle(fontSize: 18)),
+  textTheme: TextTheme(
+    bodySmall: TextStyle(fontSize: 18),
+    bodyMedium: TextStyle(fontSize: 20),
+  ),
 );
